bf052c70575ace81308289a72d9181aa468a945f
Danilburov <499732@student.fontys.nl>
2025-10-30 17:47:05 +0100

    Fix: pipeline error

--- IaC/envs/dev/vpn.tf
+++ IaC/envs/dev/vpn.tf
@@ -18,13 +18,13 @@ resource "aws_ec2_client_vpn_endpoint" "case-study-2-vpn" {
 
 //associating VPN to a subnet
 resource "aws_ec2_client_vpn_network_association" "vpn_assoc" {
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.case-study-2-vpn
   subnet_id              = aws_subnet.app[0].id
 }
 
 //auth rules
 resource "aws_ec2_client_vpn_authorization_rule" "allow_vpc" {
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.case-study-2-vpn
   target_network_cidr    = var.vpc_cidr
   authorize_all_groups   = true
   description            = "Allow access to all VPC networks"
@@ -38,7 +38,7 @@ locals {
 
 resource "aws_ec2_client_vpn_route" "to_private" {
   for_each               = toset(local.route_destinations)
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.case-study-2-vpn
   destination_cidr_block = each.value
   target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.vpn_assoc.subnet_id
   depends_on             = [aws_ec2_client_vpn_network_association.vpn_assoc]
