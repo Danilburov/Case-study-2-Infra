18dea6cd2dda882ad77095c612255bacabb316e5
Danilburov <499732@student.fontys.nl>
2025-10-30 17:37:39 +0100

    allow PostgreSQL to connect to VPN clients

--- IaC/envs/dev/vpn.tf
+++ IaC/envs/dev/vpn.tf
@@ -43,3 +43,13 @@ resource "aws_ec2_client_vpn_route" "to_private" {
   target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.vpn_assoc.subnet_id
   depends_on             = [aws_ec2_client_vpn_network_association.vpn_assoc]
 }
+#allow PostgreSQL from VPN clients
+resource "aws_security_group_rule" "vpn_to_rds" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["10.100.0.0/22"]
+  security_group_id = aws_security_group.rds.id
+  description       = "Allow Postgres from VPN clients"
+}
