56a2575be6bdcc9033c5e416dd9f43fef1faee9b
Danilburov <499732@student.fontys.nl>
2025-10-30 16:53:59 +0100

    Changed the subnet to be different for the VPN

--- IaC/envs/dev/vpn.tf
+++ IaC/envs/dev/vpn.tf
@@ -1,4 +1,3 @@
-# ---- Client VPN endpoint ----
 resource "aws_ec2_client_vpn_endpoint" "team_vpn" {
   description            = "case-study-2-client-vpn"
   server_certificate_arn = var.server_cert_arn
@@ -17,13 +16,13 @@ resource "aws_ec2_client_vpn_endpoint" "team_vpn" {
   tags = { Name = "case-study-2-vpn" }
 }
 
-//associating the VPN to one private app subnet in the same vpc
+//associating VPN to a subnet
 resource "aws_ec2_client_vpn_network_association" "vpn_assoc" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
   subnet_id              = aws_subnet.app[0].id
 }
 
-//allow VPN clients to access the entire VPC CIDR
+//auth rules
 resource "aws_ec2_client_vpn_authorization_rule" "allow_vpc" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
   target_network_cidr    = var.vpc_cidr
@@ -31,16 +30,16 @@ resource "aws_ec2_client_vpn_authorization_rule" "allow_vpc" {
   description            = "Allow access to all VPC networks"
 }
 
-//making routes from VPN to private subnets
 locals {
-  vpn_target_subnet_id = aws_ec2_client_vpn_network_association.vpn_assoc.subnet_id
-  private_cidrs        = concat(var.app_subnet_cidrs, var.data_subnet_cidrs)
+  assoc_cidr       = aws_subnet.app[0].cidr_block
+  private_cidrs    = concat(var.app_subnet_cidrs, var.data_subnet_cidrs)
+  route_destinations = [for c in local.private_cidrs : c if c != local.assoc_cidr]
 }
 
 resource "aws_ec2_client_vpn_route" "to_private" {
-  for_each               = toset(local.private_cidrs)
+  for_each               = toset(local.route_destinations)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.team_vpn.id
   destination_cidr_block = each.value
-  target_vpc_subnet_id   = local.vpn_target_subnet_id
+  target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.vpn_assoc.subnet_id
   depends_on             = [aws_ec2_client_vpn_network_association.vpn_assoc]
 }
